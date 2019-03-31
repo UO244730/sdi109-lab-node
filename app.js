4aed5aacfc64b7fff0b6d185737c7c8af9549aca
UO244730 <UO244730@uniovi.es>
2019-03-31 20:12:52 +0200

    SDI-NodeJS - 9.5 - Sistema de compras

--- app.js
+++ app.js
@@ -48,6 +48,8 @@ routerUsuarioSession.use(function(req, res, next) {
 //Aplicar routerUsuarioSession
 app.use("/canciones/agregar",routerUsuarioSession);
 app.use("/publicaciones",routerUsuarioSession);
+app.use("/cancion/comprar",routerUsuarioSession);
+app.use("/compras",routerUsuarioSession);
 
 
 //routerUsuarioAutor
@@ -84,7 +86,17 @@ routerAudios.use(function(req, res, next) {
             if(req.session.usuario && canciones[0].autor == req.session.usuario ){
                 next();
             } else {
-                res.redirect("/tienda");
+                var criterio = {
+                    usuario : req.session.usuario,
+                    cancionId : mongo.ObjectID(idCancion)
+                };
+                gestorBD.obtenerCompras(criterio ,function(compras){
+                    if (compras != null && compras.length > 0 ){
+                        next();//return
+                    } else {
+                        res.redirect("/tienda");
+                    }
+                });
             }
         })
 });
